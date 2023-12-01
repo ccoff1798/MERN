65d0ca45f9f45d73a5cc541a3066abca693473cb
ccoff1798 <135388607+ccoff1798@users.noreply.github.com>
2023-11-30 18:38:59 -0700

    auth.js server.js

--- server/server.js
+++ server/server.js
@@ -3,19 +3,50 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//Imported Apollo Server
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+//Imported TypeDefs 
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//server defined
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+//starting Apollo Server
+const startApolloServer = async () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//using graphql middlewear
+app.use('/graphql', expressMiddleware(server));
+
 // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// boilerplate
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} 
+
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+};
+
+//calling server to start
+startApolloServer();
